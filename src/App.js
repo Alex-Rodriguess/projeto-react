a1e0d5f968001b4f69255d93ff087d8be79194c9
Alex Rodriguess <alexmusicalsa@gmail.com>
2023-04-11 19:12:08 -0300

    ajustado hora

--- src/App.js
+++ src/App.js
@@ -4,13 +4,23 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Hello World!!!
-        </p>
-       
+      <header>
+        <nav className="navbar">
+            <a href="#home">Home</a>
+            <a href="#sobre">Sobre</a>
+            <a href="#services">Serviços</a>
+            <a href="#portifolio">Portfólio</a>
+            <a href="#contact">Contato</a>
+        </nav>
       </header>
+        <div className="home-content">
+            <h3>Olá, eu sou o</h3>
+            <h1>Alex Rodrigues</h1>
+            <h3>Eu sou um</h3>
+            <p>Sejam bem vindos ao meu portifólio,  fique à vontade para visualizar os meus projetos.
+                 No final da página se quiser me mandar uma mensagem, 
+                 ficarei grato!</p>
+        </div>          
     </div>
   );
 }
